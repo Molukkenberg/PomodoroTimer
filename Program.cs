0651269519fbf9a7325ddf02d006a34be9c51b43
Mika Kruckenberg <Mika.kruckenberg@gmx.de>
2024-03-22 16:48:57 +0100

    Added and configured Radzen Nuget

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using PomodoroTimer.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add Radzen Components
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
